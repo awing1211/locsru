01f0370f97a0175bed771d06d2f436aa3f7eb86e
Dacheng Xie <awing1211@wirelessprvnat-172-16-173-111.near.illinois.edu>
2013-04-08 12:48:57 -0500

    Script works
    Script works when there is no "title=," need to figure out how to
    include title and author as next step.

--- sample.rb
+++ sample.rb
@@ -1,11 +1,12 @@
 #Example search_retrieve request.
 require 'rubygems'
 require 'sru'
+require 'libxml'
 
 #An iterator for search results which allows you to do stuff like:
 
 client = SRU::Client.new 'http://z3950.loc.gov:7090/voyager',:parser=>'libxml'
-for record in client.search_retrieve('"title=harry potter"')
+for record in client.search_retrieve('"harry potter"')
    puts record
 end
 
