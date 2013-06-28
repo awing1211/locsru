64677c59ef0f0eb24a39849edd63e2cdac6f8cbe
Dacheng Xie <awing1211@Dachengs-MacBook-Pro.local>
2013-06-28 13:26:50 -0500

    Finalized search

--- sample.rb
+++ sample.rb
@@ -6,9 +6,10 @@ require 'libxml'
 #An iterator for search results which allows you to do stuff like:
 
 client = SRU::Client.new 'http://z3950.loc.gov:7090/voyager',:parser=>'libxml'
-for record in client.search_retrieve('"harry potter"')
+
+for record in client.search_retrieve('title = "To Kill a Mockingbird" and author = "Lee, Harper"', :maximumRecords => 50)
    puts record
-end
+end 
 
 puts "\n\n"
 puts "finished"
