e7c9a10feab6b8833b7718fa8d2910d13de7e72a
Dacheng Xie <awing1211@gmail.com>
2013-06-29 20:36:47 -0500

    Added non-exact keyword search

--- sample.rb
+++ sample.rb
@@ -7,7 +7,7 @@ require 'libxml'
 
 client = SRU::Client.new 'http://z3950.loc.gov:7090/voyager',:parser=>'libxml'
 
-for record in client.search_retrieve('title = "To Kill a Mockingbird" and author = "Lee, Harper"', :maximumRecords => 50)
+for record in client.search_retrieve('title = "Lord of Chaos" and author = "Jordan, Rob*"', :maximumRecords => 50)
    puts record
 end 
 
