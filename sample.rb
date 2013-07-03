8ada4bd2d3bf4ed7777c4ef7f780068608713555
Dacheng Xie <awing1211@gmail.com>
2013-07-02 21:51:23 -0500

    Saved result to a local xml file

--- sample.rb
+++ sample.rb
@@ -6,9 +6,11 @@ require 'libxml'
 #An iterator for search results which allows you to do stuff like:
 
 client = SRU::Client.new 'http://z3950.loc.gov:7090/voyager',:parser=>'libxml'
-
 for record in client.search_retrieve('title = "Lord of Chaos" and author = "Jordan, Rob*"', :maximumRecords => 50)
-   puts record
+   File.open('sample.xml', 'w') do|file|
+     file.write(record)
+   end
+   
 end 
 
 puts "\n\n"
